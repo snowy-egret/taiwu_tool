8ce77c2c6d19128e89a4dc53584ad190e7af6be1
Snowyegret <snowyegret98@gmail.com>
2023-06-30 23:21:27 +0900

    update diff_text tool

--- diff_text/main.py
+++ diff_text/main.py
@@ -1,65 +1,68 @@
 import os
 
+def printwrite(text):
+    log.write(f"{text}\n")
+    print(text)
 
 def difffile(old, new):
+    # 구버전 텍스트 파일 읽기
     with open(f"txt_files_old/{old}", "r", encoding="utf-8") as f:
-        old_lines = f.readlines()
-        old_lines = [i.strip() for i in old_lines]
+        old_lines = [i.strip() for i in f.readlines()]
         old_lines = [i for i in old_lines if i != ""]
 
+    # 신버전 텍스트 파일 읽기
     with open(f"txt_files_new/{new}", "r", encoding="utf-8") as f:
-        new_lines = f.readlines()
-        new_lines = [i.strip() for i in new_lines]
+        new_lines = [i.strip() for i in f.readlines()]
         new_lines = [i for i in new_lines if i != ""]
 
+    # 구버전에만 있는 텍스트 / 신버전에만 있는 텍스트 구하기
     only_old = [i for i in old_lines if i not in new_lines]
     only_new = [i for i in new_lines if i not in old_lines]
 
+    # Error Handling: 차이점이 없을 경우 패스
     if len(only_old) == 0 and len(only_new) == 0:
         return None
+    
+    # 파일 이름 전처리
     try:
         filename = os.path.splitext(new)[0].split('-CAB')[0]
-        log.write(f"파일 이름: {filename}\n")
-        print(f"파일 이름: {filename}")
-        
     except:
         filename = os.path.splitext(new)[0]
-        log.write(f"파일 이름: {filename}\n")
-        print(f"파일 이름: {filename}")
+
+    # 로그 작성
+    printwrite(f"파일 이름: {filename}")
     if len(only_old) != 0:
-        log.write("\t구버전에만 있는 텍스트:\n")
-        print("\t구버전에만 있는 텍스트:")
+        printwrite("\t구버전에만 있는 텍스트:")
         for i in only_old:
-            log.write(f"\t{i}\n")
-            print(f"\t{i}")
-        log.write("\n")
-        print("")
+            printwrite(f"\t{i}")
+        printwrite("")
     if len(only_new) != 0:
-        log.write("\t신버전에만 있는 텍스트:\n")
-        print("\t신버전에만 있는 텍스트:")
+        printwrite("\t신버전에만 있는 텍스트:")
         for i in only_new:
-            log.write(f"\t{i}\n")
-            print(f"\t{i}")
-    log.write("\n\n")
-    print("\n")
+            printwrite(f"\t{i}")
+    printwrite("\n")
 
-log = open("log.txt", "w", encoding="utf-8")
+if __name__ == "__main__":
+    # 작업 디렉토리 설정
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-old_list = [i for i in os.listdir("txt_files_old") if i.endswith(".txt")]
-new_list = [i for i in os.listdir("txt_files_new") if i.endswith(".txt")]
+    log = open("log.txt", "w", encoding="utf-8")
 
-if len(old_list) != len(new_list):
-    print("파일 개수가 다릅니다.\n")
-    print(f"구버전: {len(old_list)}개\n")
-    print(f"신버전: {len(new_list)}개\n")
-    input("아무 키나 눌러 종료하세요...")
+    # 구버전 / 신버전 텍스트 파일 목록 구하기
+    old_list = [i for i in os.listdir("./txt_files_old") if i.endswith(".txt")]
+    new_list = [i for i in os.listdir("./txt_files_new") if i.endswith(".txt")]
+
+    # Error Handling: 파일 개수가 다를 경우
+    if len(old_list) != len(new_list):
+        printwrite("파일 개수가 다릅니다.")
+        printwrite(f"구버전: {len(old_list)}개")
+        printwrite(f"신버전: {len(new_list)}개")
+        log.close()
+        input("아무 키나 눌러 종료하세요...")
+        exit()
+
+    for i in range(len(old_list)):
+        difffile(old_list[i], new_list[i])
+
     log.close()
-    exit()
-
-for i in range(len(old_list)):
-    difffile(old_list[i], new_list[i])
-
-
-
-log.close()
-input("작업을 완료했습니다. 종료하려면 엔터를 누르세요...")
+    input("작업을 완료했습니다. 종료하려면 엔터를 누르세요...")
